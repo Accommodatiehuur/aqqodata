cde086311e92d1a9a5f987086aff93e322b36810
Max Hendriks <max@aqqo.com>
2024-11-18 13:50:45 +0100

    Add setup for CountTest

--- tests/Testcase.php
+++ tests/Testcase.php
@@ -3,14 +3,35 @@
 namespace Aqqo\OData\Tests;
 
 use Aqqo\OData\ServiceProvider;
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class Testcase extends \Orchestra\Testbench\TestCase
 {
+    use DatabaseMigrations;
 
     protected function getPackageProviders($app)
     {
         return [
             ServiceProvider::class,
         ];
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->setUpDatabase($this->app);
+    }
+
+    protected function setUpDatabase(?Application $app): void
+    {
+        if (is_null($app)) {
+            return;
+        }
+
+        $app['db']->connection()->getSchemaBuilder()->create('test_models', function ($table) {
+            $table->increments('id');
+        });
     }
 }
